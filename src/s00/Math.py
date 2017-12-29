f3753f6d9a44bf25c81ee1570f38b3971215692e
Heidi <discreteneko@outlook.com>
2017-12-29 11:13:49 +0800

    Add framwork of unittest

---
+++ src/s00/Math.py
@@ -0,0 +1,15 @@
+import math
+
+class Math:
+
+    def solver(self, a, b, c):
+        d = b ** 2 - 4 * a * c
+        if d >= 0:
+            disc = math.sqrt(d)
+            root1 = (-b + disc) / (2 * a)
+            root2 = (-b - disc) / (2 * a)
+            print(root1, root2)
+        else:
+            raise Exception
+
+Math().solver(2, 1, 0)
---
+++ test/test_s00/test_math.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from src.s00.Math import Math
+
+
+class TestSolver(TestCase):
+
+    def test_negative_discr(self):
+        s = Math()
+        self.assertRaise(Exception,s.solver,2,1,2)
